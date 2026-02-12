35751f5d5e208fc02d56cc2a2c5a71376d215f71
Pablo Arturo <pabloarturo2209@gmail.com>
2026-02-12 11:01:32 -0600

    Implement the search command

--- dart/dart-tutorial/dartpedia/cli/bin/cli.dart
+++ dart/dart-tutorial/dartpedia/cli/bin/cli.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 const version = '0.0.1';
 
 void main(List<String> arguments) {
@@ -5,9 +7,30 @@ void main(List<String> arguments) {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
     printUsage();
   }
+}
+
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  // If the user didn't pass in arguments, request an article title.
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    // Await input and provide a default empty string if the input is null.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // Otherwise, join the arguments into the CLI into a single string
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
 }
 
 void printUsage() {
